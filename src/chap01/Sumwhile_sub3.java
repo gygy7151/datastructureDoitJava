958c56a0af3a0e07ca31aa518a40171803679fa5
Mac <gygy7151@gmail.com>
2021-04-21 21:11:21 +0900

    210421_연습문제9번_풀이완료
    a와b의 대소 관계에 상관없이 합을 구하는 코드/ a와b중 음수 및 0인경우도 포함하는 정수의 합 구하기

--- src/chap01/Sumwhile_sub3.java
+++ src/chap01/Sumwhile_sub3.java
@@ -3,49 +3,43 @@ package chap01;
 import java.util.Scanner;
 
 
-//연습문제 Q6
+//연습문제 Q9
 
-public class Sumwhile_sub3 {		
-	static void sumof(int a, int b) {
-		if (a > b)  {
-			int sum = b;
-			int i = b;
+public class Sumwhile_sub3 {				
+	static int sumof(int a, int b) {
 			
-			while (i <= a) {
-				sum += i;  //sum에 i를 더합니다. 
-				i++; 
-				return;
-			}
+			int sum = 0;
 			
-			System.out.println("a와 b사이의 모든 정수의 합은" + sum + "입니다." );
-			
-			
-		} else if (b < a) {
-				int sum = a;
-				int i = a;
+			if (a > b) {
 				
-				while (i <= b) {
-					sum += i;
-					i++;
-					return;
-				}
+				for (int i = b; i <= a; i++)
+					sum +=i;
 				
-				System.out.println("a와 b사이의 모든 정수의 합은" + sum + "입니다." );
+			}else if (a < b) {
+				for (int i = a; i <= b; i++)
+					sum +=i;
 				
 			}
+			return sum;
 
+		
 		}
 	
-	public static void main(String[] args) {
-		Scanner stdIn = new Scanner(System.in);
-		
-		System.out.println("a와 b사이의 모든 정수의 합을 구합니다.");
-		System.out.print("a의 값 : ");
-		int a = stdIn.nextInt();
-		System.out.print("b의 값 : ");
-		int b = stdIn.nextInt();
-		
+		public static void main(String[] args) {
+			Scanner stdIn = new Scanner(System.in);
+			
+			System.out.println("a와 b를 포함하여 그사이 모든 정수의 합을 구합니다.");
+			System.out.print("a의 값 : ");
+			int a = stdIn.nextInt();
+			System.out.print("b의 값 : ");
+			int b = stdIn.nextInt();
+			
+			System.out.println("a와 b를 포함한 그사이의 정수의 합" + sumof(a,b) + "입니다.");
+		}
 	}
+
+
+// 첫번째 접근
 //		if (a > b) {
 //			int n = b;
 //			int sum = b;
@@ -68,5 +62,31 @@ public class Sumwhile_sub3 {
 		//1부터 n까지의 합을 구합니다.
 //		n의 값 : 8
 //		1부터 8까지의 합은 8번째 합인 36입니다.
+// => 결과 안나옴
 
-}
+// 두번째 방법
+//		if (a > b)  {
+//int sum = b;
+//int i = b;
+//
+//while (i <= a) {
+//	sum += i;  //sum에 i를 더합니다. 
+//	i++; 
+//	return;
+//}
+//
+//System.out.println("a와 b사이의 모든 정수의 합은" + sum + "입니다." );
+//
+//
+//} else if (b < a) {
+//	int sum = a;
+//	int i = a;
+//	
+//	while (i <= b) {
+//		sum += i;
+//		i++;
+//		return;
+//	}
+//	
+//	System.out.println("a와 b사이의 모든 정수의 합은" + sum + "입니다." );
+
